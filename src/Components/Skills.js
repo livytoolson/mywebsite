8fb9a2b787d033d3e16be7bd54bccc2099b1c9e7
Olivia Toolson <livytoolson@gmail.com>
2021-04-02 19:28:24 -0600

    added skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,9 +1,22 @@
-import { React } from 'react';
+import React from 'react';
 
 const Skills = () => {
     return (
-        <div>
-
+        <div className="skills">
+            <p>HTML</p>
+            <p>CSS</p>
+            <p>JavaScript</p>
+            <p>React</p>
+            <p>Redux</p>
+            <p>Node / Express Framework</p>
+            <p>PostreSQL / SQLite3</p>
+            <p>Python</p>
+            <p>Git / GitHub</p>
+            <p>VS Code / Postman</p>
+            <p>Heroku / Vercel / Netlify</p>
+            <p>Ant Design / Styled Components</p>
+            <p>UX / UI</p>
+            <p>Whimsical / Figma</p>
         </div>
     );
 };
