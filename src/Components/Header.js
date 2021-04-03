36cbd843fe4e2e8fcbf97ad4289e22563c395dca
Olivia Toolson <livytoolson@gmail.com>
2021-04-02 19:28:13 -0600

    added onClicks and hrefs

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,24 @@
-import { React } from 'react';
+import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Header = () => {
+    const { push } = useHistory();
+
+    const onHome = () => {
+        push('/')
+    };
+
+    const onAbout = () => {
+        push('/about')
+    };
+
     return (
         <div className="header">
             <p className="header-name">olivia</p>
             <div className="header-links">
-                <a href="">home</a>
-                <a href="">about</a>
-                <a href="">resume</a>
+                <a href="#" onClick={onHome}>home</a>
+                <a href="#" onClick={onAbout}>about</a>
+                <a href="https://drive.google.com/file/d/17Eci66AR75ECoFH68RNUaFYNouhyOC4w/view">resume</a>
             </div>
         </div>
     );
